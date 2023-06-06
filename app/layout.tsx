60601a3b6ac7184c0787358e6b264effa6bfbc36
Sarath Tharayil <iamsaraththarayil@gmail.com>
2023-06-06 06:48:09 +0100

    6th

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
 	title: {
-		default: "chronark.com",
-		template: "%s | chronark.com",
+		default: "Sarath Tharayil",
+		template: "%s | Sarath Tharayil",
 	},
-	description: "Software engineer at upstash.com and founder of planetfall.io",
+	description: "Data Science student @ The University of Sheffield",
 	openGraph: {
-		title: "chronark.com",
+		title: "Sarath Tharayil",
 		description:
-			"Software engineer at upstash.com and founder of planetfall.io",
-		url: "https://chronark.com",
-		siteName: "chronark.com",
+			"Data Science student @ The University of Sheffield",
+		url: "https://www.saraththarayil.com",
+		siteName: "saraththarayil.com",
 		images: [
 			{
-				url: "https://chronark.com/og.png",
+				url: "https://saraththarayil.com/og.png",
 				width: 1920,
 				height: 1080,
 			},
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
 		},
 	},
 	twitter: {
-		title: "Chronark",
+		title: "Sarath Tharayil",
 		card: "summary_large_image",
 	},
 	icons: {
