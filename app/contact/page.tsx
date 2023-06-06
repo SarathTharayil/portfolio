4a9afe64e1419e7f2b87b59f6b33971f57079695
Sarath Tharayil <iamsaraththarayil@gmail.com>
2023-06-06 06:57:11 +0100

    7th

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/SarathTharayil",
+		href: "https://twitter.com/chronark_",
 		label: "Twitter",
-		handle: "@SarathTharayil",
+		handle: "@chronark_",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@saraththarayil.com",
+		href: "mailto:dev@chronark.com",
 		label: "Email",
-		handle: "dev@saraththarayil.com",
+		handle: "dev@chronark.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/saraththarayil",
+		href: "https://github.com/chronark",
 		label: "Github",
-		handle: "saraththarayil",
+		handle: "chronark",
 	},
 ];
 
