ea97e3dd1ffa0c5c0ab686d941872a99dab52bcc
Sarath Tharayil <iamsaraththarayil@gmail.com>
2023-06-10 02:07:42 +0100

    fixes?

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-	{ name: "Blog", href: "/projects" },
+	{ name: "Posts", href: "/projects" },
 	{ name: "Contact", href: "/contact" },
 ];
 
